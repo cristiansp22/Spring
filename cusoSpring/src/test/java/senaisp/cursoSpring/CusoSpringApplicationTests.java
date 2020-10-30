6589fc4c08ba305c37a52e3e8a9bcfae76a99d2d
Cristian <cristiansilva2019@gmail.com>
2020-10-30 10:16:59 -0200

    Prod Base

---
+++ cusoSpring/src/test/java/senaisp/cursoSpring/CusoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package senaisp.cursoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CusoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
